462c8af9c75a917099c6085d5a03ba28e5853442
Lucas-Maurice Stein <lms@koempf24.de>
2026-04-17 11:56:21 +0200

    added open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,26 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-    title: 'Alles Gute zum Geburtstag',
-    description: 'Eine kleine Geburtstags-Überraschung',
+    metadataBase: new URL('https://alinaist23.de'),
+    title: 'Alles Gute zum Geburtstag 💙',
+    description: 'Eine kleine digitale Überraschung voller Erinnerungen, lieber Worte und besonderer Momente.',
+    openGraph: {
+        title: 'Alles Gute zum Geburtstag 💙',
+        description:
+            'Eine kleine digitale Überraschung voller Erinnerungen, lieber Worte und besonderer Momente.',
+        url: 'https://alinaist23.de',
+        siteName: 'Geburtstags-Überraschung',
+        locale: 'de_DE',
+        type: 'website',
+        images: [
+            {
+                url: '/opengraph-image.jpg',
+                width: 1200,
+                height: 630,
+                alt: 'Geburtstags-Überraschung in Blau',
+            },
+        ],
+    },
 }
 
 export default function RootLayout({
